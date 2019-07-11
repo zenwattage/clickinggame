69b09786dfe85f46d8bcc0f904bf7cb99e1a218a
Scott H <zenwattage@gmail.com>
2019-07-10 17:18:54 -0700

    adding colors and jumbotron

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import Jumbotron from "./components/Jumbotron";
 import Button from '@material-ui/core/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+       
+       <Jumbotron />
+        
           <Button variant="contained" color="primary">
             Clicky Clackity
           </Button>
-        </a>
-      </header>
+        
   </div>
   );
 }
+
+
 
 export default App;
--- src/components/Jumbotron/jumbotron.js
+++ src/components/Jumbotron/jumbotron.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Jumbotron.css";
 
-const Jumbotron = () => {
+const Jumbotron = () => (
     <header className="header">
         <h1>Clickity Clackity!</h1>
         <h1>Don't click the same image twice!</h1>
     </header>
-};
+);
 
 export default Jumbotron;
