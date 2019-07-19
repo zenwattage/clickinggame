5ef908fecd0d86e420611529dbeda2c604dde72d
Scott H <zenwattage@gmail.com>
2019-07-19 10:26:35 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -2,76 +2,58 @@ import React, { Component } from 'react';
 import Wrapper from "./components/Wrapper";
 import './App.css';
 import Card from "./components/Card";
-import cards from "./photo.json"
+import Header from "./components/Header";
+import photos from "./photos.json"
+const log = console.log;
 
 class App extends Component {
   
   state = {
-    cards,
+    photos,
     score: 0,
     highscore: 0
   };
 
-  //image click even handler
-  imageClick = event => {
-    let currentHighScore = this.state.highscore;
-    const currentPhoto = event.target;
-    const clickedPhotos = this.state.clickedPhoto.indexOf(currentPhoto);
-
-    if(clickedPhotos) {
-      if(this.state.score > currentHighScore) {
-        currentHighScore = this.state.score
-      } this.setState({
-        photo: this.state.photo.sort(function(a , b) {
-          return 0.5 - Math.random();
-        }),
-        highscore: currentHighScore,
-        clickedPhoto: [],
-        score: 0
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({ highscore: this.state.score }, function() {
+        log(this.state.highscore);
       });
-        alert("You lose! Try again");
-    } else {
-      this.setState(
-        {
-          photo: this.state.photo.sort(function(a,b) {
-            return 0.5 - Math.random();
-          }),
-          clickedPhoto: this.state.clickedPhoto.concat(
-            currentPhoto
-          ),
-          score: this.state.score + 1,
-        },
-
-        () => {
-          if(this.state.score === 9) {
-            alert("You win!");
-            this.setState({
-              photo: this.state.photo.sort(function(a,b) {
-                return 0.5 - Math.random();
-              }),
-              clickedPhoto: [],
-              score: 0,
-              highscore: 0
-            });
-          }
-        }
-      );
     }
-};
+    this.state.photos.forEach(photo => {
+      photo.count = 0;
+    });
+    alert(`Game Over :( \n score: ${this.state.score}`);
+    this.setState({ score: 0 });
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.photos.find((o, i) => {
+      if (o.id === id) {
+        if (photos[i].count === 0) {
+          photos[i].count = photos[i].count + 1;
+          this.setState({ score: this.state.score + 1}, function() {
+            log(this.state.score);
+          });
+          this.state.photos.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
 
   render() {
     return (
       <Wrapper>
-     
-          <Button> Here we go!</Button>
-        {this.state.photo.map(photo => (
-          < Card
-            imageClick = {this.imageClick}
-            id={photo.id}
-            key={photo.id}
-            photo={photo.image}
-            />
-        ))}
+        <Card 
+          clickCount={ this.clickCount }
+          id={ photo.id }
+          key={ photo.id }
+          image={ photo.image }
+          />
       </Wrapper>
     );
   }
